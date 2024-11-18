f0e30ad4a70b039c2cc97544fce39a60e9cba730
Nagaraj Patil <nagarajpatil@Nagarajs-MBP.hsd1.wa.comcast.net>
2024-11-17 20:38:24 -0800

    Initial changes

---
+++ gurukul/gurukulApp.swift
@@ -0,0 +1,17 @@
+//
+//  gurukulApp.swift
+//  gurukul
+//
+//  Created by Nagaraj Patil on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct gurukulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
