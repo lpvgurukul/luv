427002ae6e1034fde8d21b18a261678f680947bc
Naga <gurukul.volunteer@outlook.com>
2024-11-24 18:44:23 -0800

    updated feedback and contact

--- gurukul/ContentView.swift
+++ gurukul/ContentView.swift
@@ -10,11 +10,16 @@ import WebKit
 
 struct ContentView: View {
     var body: some View {
+        let email = "parents.committee@gurukul-wa.org"
+        
         VStack{
-            Text("Welcome to Gurukul LPV")
-        }
+            Text("Welcome to Gurukul WA USA")
             GurukulPage(url: URL(string: "https://gurukulweb.sharepoint.com/pages/myHomePage.aspx")!)
+                .frame(height:650)
                 .edgesIgnoringSafeArea(.all)
+            Link("Contact Parents Committee",
+                           destination: URL(string: "mailto:\(email)")!)
+        }
     }
 }
 
